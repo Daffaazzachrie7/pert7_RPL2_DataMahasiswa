2c75890c8298e701a5a28d062d682a74e134b519
Daffaazzachrie7 <Daffaazzachrie@gmail.com>
2025-12-04 20:23:15 +0700

    first commit

---
+++ pert3rpl_50422369/src/main/java/com/mycompany/pert3rpl_50422369/Pert3rpl_50422369.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert3rpl_50422369;
+
+/**
+ *
+ * @author daffa azzachrie
+ */
+public class Pert3rpl_50422369 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
